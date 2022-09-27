448ed8a22164c6d308343d0ea4344ce2d18b3682
Bánó Balázs <banobalazs@BaluMac.local>
2022-09-27 20:12:59 +0200

    create spinner

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Card } from "react-bootstrap";
 import { searchMovies } from "../../api/searchMovies";
+import ClipLoader from "react-spinners/ClipLoader";
 import "./style.css"
 
 export function HomePage(){
   const [title, setTitle] = useState("");
+  const [loading, setLoading] = useState(false);
 
   var handleKeyDown = function(e) {
     if(e.key === "Enter"){
@@ -11,16 +14,27 @@ export function HomePage(){
     }
   }
 
-  function handleSubmit(event){
+  async function handleSubmit(event){
     event.preventDefault();
-    searchMovies(title)
+    setLoading(true);
+    console.log((await searchMovies(title)).data.searchMovies);
+    setLoading(false);
   }
 
   return(
-    <div className="finder-container">
-        <h2>Movie finder</h2>
-        <input type="text" id="inputfield" placeholder="Movie title" autoFocus onChange={(e) => setTitle(e.target.value)} onKeyDown={(e) => handleKeyDown(e)}/>
-        <button type="submit" id="searchButton" onClick={(e) => handleSubmit(e)}>Search</button>
+    <div>
+      {
+        loading?
+        <div className="loader-container">
+          <ClipLoader color={"#6d7b7"} loading={loading} size={100} />
+        </div>
+        :
+        <div className="finder-container">
+          <h2>Movie finder</h2>
+          <input type="text" id="inputfield" placeholder="Movie title" autoFocus onChange={(e) => setTitle(e.target.value)} onKeyDown={(e) => handleKeyDown(e)}/>
+          <button type="submit" id="searchButton" onClick={(e) => handleSubmit(e)}>Search</button>
+        </div>
+      }
     </div>
   )
 }
