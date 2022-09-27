3afdd52791998e7fbcceb5eb9847f9be3b45601a
Bánó Balázs <banobalazs@BaluMac.local>
2022-09-27 13:55:01 +0200

    create onclick event with variable in state

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,20 @@
-import { Button, Card } from "react-bootstrap";
+import { useState } from "react";
 import { searchMovies } from "../../api/searchMovies";
 import "./style.css"
 
 export function HomePage(){
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(event){
+    event.preventDefault();
+    searchMovies(title)
+  }
+
   return(
     <div className="finder-container">
-      <h2>Movie finder</h2>
-      <input type="text" id="input" placeholder="Movie title" autoFocus/>
-      <button>Search</button>
+        <h2>Movie finder</h2>
+        <input type="text" id="input" placeholder="Movie title" autoFocus onChange={(e) => setTitle(e.target.value)}/>
+        <button type="submit" onClick={(e) => handleSubmit(e)}>Search</button>
     </div>
   )
 }
