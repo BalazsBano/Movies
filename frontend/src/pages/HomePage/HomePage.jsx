9e38622c323036eb6726166d78849bc9c71d13cf
Bánó Balázs <banobalazs@BaluMac.local>
2022-09-27 14:19:56 +0200

    adding search to press enter

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,12 @@ import "./style.css"
 export function HomePage(){
   const [title, setTitle] = useState("");
 
+  var handleKeyDown = function(e) {
+    if(e.key === "Enter"){
+      handleSubmit(e)
+    }
+  }
+
   function handleSubmit(event){
     event.preventDefault();
     searchMovies(title)
@@ -13,8 +19,8 @@ export function HomePage(){
   return(
     <div className="finder-container">
         <h2>Movie finder</h2>
-        <input type="text" id="input" placeholder="Movie title" autoFocus onChange={(e) => setTitle(e.target.value)}/>
-        <button type="submit" onClick={(e) => handleSubmit(e)}>Search</button>
+        <input type="text" id="inputfield" placeholder="Movie title" autoFocus onChange={(e) => setTitle(e.target.value)} onKeyDown={(e) => handleKeyDown(e)}/>
+        <button type="submit" id="searchButton" onClick={(e) => handleSubmit(e)}>Search</button>
     </div>
   )
 }
